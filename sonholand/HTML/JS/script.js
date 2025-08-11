8376231d6d012dd872eba78aa5bbbc1970fdb020
Matheus César <146596138+Matheus0197M@users.noreply.github.com>
2025-08-11 20:33:49 -0300

    feat::atualização geral do projeto

---
+++ sonholand/HTML/JS/script.js
@@ -0,0 +1,38 @@
+const uberlandia = {
+    nome: "Uberlândia",
+    habitantes: 800000,
+    idade: 151,
+}
+
+const uberaba = {
+    nome: "Uberaba",
+    habitantes: 200000,
+    idade: 171
+}
+
+const araguari = {
+    nome: "Araguari",
+    habitantes: 300000,
+    idade: 182
+}
+
+function verificarCidade() {
+    if (uberlandia.habitantes > 300000) {
+        console.log("É uma cidade grande!")
+    } else {
+        console.log("Ainda é uma cidade pequena.")
+    }
+
+}
+
+verificarCidade()
+
+/*
+const Uberlandia = 800000;
+const Uberaba = 300000;
+const PatosMinas = 455000;
+const Paracatu = 210000;
+const Araguari = 100000; 
+const BeloHorizonte = 1000000;
+
+*/
